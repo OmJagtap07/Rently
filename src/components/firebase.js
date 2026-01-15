04aaf76009df475be9ca29b93a8fba780a439709
Om Jagtap <omjagtapatil07@gmail.com>
2026-01-15 21:55:18 +0530

    Add: env to secure my api keys

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // REPLACE THIS OBJECT WITH YOUR ACTUAL KEYS FROM FIREBASE CONSOLE
 const firebaseConfig = {
-    apiKey: "AIzaSyAPcDxKbxgXtigeF4ivYzqUaAQ_4hKU09c",
-    authDomain: "rently-343c2.firebaseapp.com",
-    projectId: "rently-343c2",
-    storageBucket: "rently-343c2.firebasestorage.app",
-    messagingSenderId: "361130905190",
-    appId: "1:361130905190:web:dfa8b7a35bb9eae79427a7",
-    measurementId: "G-Y4WSZ3DSVM"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
